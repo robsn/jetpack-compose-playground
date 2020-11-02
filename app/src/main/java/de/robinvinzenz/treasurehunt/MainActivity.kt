f5ccd82fcb5ea73a495b25fbc9575525c4b22ad9
Robin Vinzenz <995962+robsn@users.noreply.github.com>
2020-11-02 17:22:07 +0100

    Init commit

---
+++ app/src/main/java/de/robinvinzenz/treasurehunt/MainActivity.kt
@@ -0,0 +1,28 @@
+package de.robinvinzenz.treasurehunt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Hello world!")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text (text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Android")
+}
